ab67e1e530d636ffdbbb375288ad503a34d0e212
Gheorghe Morari <53918731+GheorgheMorari@users.noreply.github.com>
2023-10-30 23:49:07 +0200

    Add ViewModel filtering

--- app/src/main/java/com/example/asslab/ViewModel.java
+++ app/src/main/java/com/example/asslab/ViewModel.java
@@ -13,17 +13,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import org.tensorflow.lite.support.image.TensorImage;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.function.Consumer;
 import java.util.stream.IntStream;
 
 public class ViewModel {
-    // Contains all the images
-    // Initiates the inference
-    // Filters the images
-    // Sends the images back to the view
     ArrayList<Bitmap> filteredImages;
     ActivityResultLauncher<Intent> selectionActivityLauncher;
     AppCompatActivity activity;
@@ -32,13 +30,33 @@ public class ViewModel {
     Consumer<ArrayList<Bitmap>> updateViewCallback;
     ModelClass modelClass;
     boolean processedFlag = false;
+    String[] labels;
 
     public ViewModel(AppCompatActivity activity, Consumer<ArrayList<Bitmap>> updateViewCallback) {
+
         this.activity = activity;
         this.updateViewCallback = updateViewCallback;
         selectionActivityLauncher = activity.registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), this::onSelectFinished);
         modelClass = new ModelClass(activity);
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(activity.getAssets().open("labels.txt")));
+            int labelCount = 1001;
+
+            labels = new String[labelCount];
+
+            int i = 0;
+            String line;
+
+            while ((line = bufferedReader.readLine()) != null && i < labelCount) {
+                labels[i] = line;
+                i++;
+            }
+            bufferedReader.close(); // Close the reader when done.
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
 
     void onSelectFinished(ActivityResult result) {
         if (result.getResultCode() == Activity.RESULT_OK) {
@@ -84,8 +102,29 @@ public class ViewModel {
             updateViewCallback.accept(selectedImages); // Show all images if it's not done processing or the class name is empty
             return;
         }
-        //TODO
-        updateViewCallback.accept(selectedImages);
+        filteredImages = new ArrayList<>();
+        int minDistance = Integer.MAX_VALUE;
+        int closestLabelIndex = 0;
+        float detectionThreshold = 10.0f;
+
+        for (int i = 0; i < labels.length; i++) {
+            int distance = modifiedLevenshteinDistance(className, labels[i]);
+            if (distance < minDistance) {
+                minDistance = distance;
+                closestLabelIndex = i;
+            }
+        }
+        System.out.println("Closest label:" + labels[closestLabelIndex]);
+
+        for (int i = 0; i < selectedImagesOutput.size(); i++) {
+            float labelValue = selectedImagesOutput.get(i)[closestLabelIndex];
+            if (labelValue >= detectionThreshold) {
+                filteredImages.add(selectedImages.get(i));
+            }
+            System.out.println("Label value: " + labelValue + " " + i);
+        }
+
+        updateViewCallback.accept(filteredImages);
     }
 
     void processImagesAsync() {
@@ -99,8 +138,44 @@ public class ViewModel {
                 selectedImagesOutput.add(modelClass.run(imageByteBuf));
             }
             modelClass.free();
+
+            for (int i = 0; i < selectedImagesOutput.size(); i++) {
+                float labelValue = 0;
+                int maxValueIndex = 0;
+                for (int j = 0; j < labels.length; j++) {
+                    float currentValue = selectedImagesOutput.get(i)[j];
+                    if (currentValue > labelValue) {
+                        labelValue = currentValue;
+                        maxValueIndex = j;
+                    }
+                }
+                System.out.println("Label value for image " + i + " is " + labelValue + " for label:" + labels[maxValueIndex]);
+            }
+
             processedFlag = true;
         }).start();
     }
 
+    private static int modifiedLevenshteinDistance(String str1, String str2) {
+        int[][] dp = new int[str1.length() + 1][str2.length() + 1];
+
+        for (int i = 0; i <= str1.length(); i++) {
+            for (int j = 0; j <= str2.length(); j++) {
+                if (i == 0) {
+                    dp[i][j] = j;
+                } else if (j == 0) {
+                    dp[i][j] = i;
+                } else {
+                    int cost = (str1.charAt(i - 1) == str2.charAt(j - 1)) ? 0 : 1;
+                    dp[i][j] = Math.min(Math.min(
+                                    dp[i - 1][j] + 1, // Deletion in str1
+                                    dp[i][j - 1] + 1), // Deletion in str2
+                            dp[i - 1][j - 1] + cost);
+                }
+            }
+        }
+
+        return dp[str1.length()][str2.length()];
+    }
 }
+
